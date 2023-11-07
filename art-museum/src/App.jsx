ffc42faa71edf6a2c127aa3902377cdb615a21c4
Azadcelik <azadcelikk12@gmail.com>
2023-11-07 16:22:10 -0500

    first-commit

---
+++ art-museum/src/App.jsx
@@ -0,0 +1,16 @@
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import  harvardArt from './data/harvardArt.js'
+
+const router = createBrowserRouter([
+  { 
+    path: '*',
+    element : <h2>Page Not Found</h2>
+  }
+])
+
+
+function App() {
+  return <RouterProvider router={router}/>;
+}
+
+export default App;
